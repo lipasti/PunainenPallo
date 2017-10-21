f06644589322a2ceb02246e32fa356a4a9b9a968
It-guest <It-guest@FINLAND03>
2017-10-21 13:22:40 +0300

    Pelaajan ohjaus toimii

--- PunainenPallo/PunainenPallo/PunainenPallo.cs
+++ PunainenPallo/PunainenPallo/PunainenPallo.cs
@@ -8,26 +8,38 @@ using Jypeli.Widgets;
 
 public class PunainenPallo : PhysicsGame
 {
+    int pelaajanKoko = 70;
+    int pelaajanNopeus = 200;
+    Vector pelaajanAloituspaikka = new Vector(100, 100);
     Image pelaajaKuva = LoadImage("Pelaaja");
+
     PhysicsObject pelaaja;
     public override void Begin()
     {
         LuoPelaaja();
 
+        Keyboard.Listen(Key.Up, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa ylös", new Vector(0, pelaajanNopeus));
+        Keyboard.Listen(Key.Up, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
+        Keyboard.Listen(Key.Down, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa alas", new Vector(0, -pelaajanNopeus));
+        Keyboard.Listen(Key.Down, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
+        Keyboard.Listen(Key.Right, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa oikealle", new Vector(pelaajanNopeus, 0));
+        Keyboard.Listen(Key.Right, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
+        Keyboard.Listen(Key.Left, ButtonState.Down, LiikutaPelaajaa, "Pelaajaa vasemmalle", new Vector(-pelaajanNopeus, 0));
+        Keyboard.Listen(Key.Left, ButtonState.Released, LiikutaPelaajaa, null, Vector.Zero);
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
     void LuoPelaaja()
     {
-        pelaaja = new PhysicsObject(50, 50, Shape.Circle);
-
-        pelaaja.X = 100;
-        pelaaja.Y = 100;
+        pelaaja = new PhysicsObject(pelaajanKoko, pelaajanKoko, Shape.Circle);
+        pelaaja.Position = pelaajanAloituspaikka;
         pelaaja.Image = pelaajaKuva;
+        Add(pelaaja);
+    }
 
-       
-       Add(pelaaja);
-
+    void LiikutaPelaajaa(Vector suunta)
+    {
+        pelaaja.Move(suunta);
     }
 }
